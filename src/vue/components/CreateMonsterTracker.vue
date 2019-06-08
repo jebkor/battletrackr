c1d73d1f90df0d9e3b2f51ac97aeef9ef06f4fb5
jebkor <jesper@jebkor.dk>
2019-06-08 23:46:05 +0200

    Add more lint changes

--- src/vue/components/CreateMonsterTracker.vue
+++ src/vue/components/CreateMonsterTracker.vue
@@ -1,79 +1,80 @@
 <template>
-	<v-flex
-		xs12
-		sm6
-		lg3
-		class="create-encounter-tracker"
-	>
-		<v-card>
-			<v-card-text>
-				<v-form
-					model="valid"
-					@submit.prevent="addMonster()"
-				>
-					<v-text-field
-						v-model="name"
-						label="Name"
-						solo
-						required
-					/>
+  <v-flex
+    xs12
+    sm6
+    lg3
+    class="create-encounter-tracker"
+  >
+    <v-card>
+      <v-card-text>
+        <v-form
+          model="valid"
+          @submit.prevent="addMonster()"
+        >
+          <v-text-field
+            v-model="name"
+            label="Name"
+            solo
+            required
+          />
 
-					<v-text-field
-						v-model="maxHealth"
-						label="Hit Points"
-						type="number"
-						solo
-						required
-					/>
+          <v-text-field
+            v-model="maxHealth"
+            label="Hit Points"
+            type="number"
+            solo
+            required
+          />
 
-					<v-checkbox
-						v-model="isBoss"
-						:label="'Boss monster?'"
-					/>
+          <v-checkbox
+            v-model="isBoss"
+            :label="'Boss monster?'"
+          />
 
-					<v-btn type="submit">Add monster</v-btn>
-				</v-form>
-			</v-card-text>
-		</v-card>
-	</v-flex>
+          <v-btn type="submit">
+            Add monster
+          </v-btn>
+        </v-form>
+      </v-card-text>
+    </v-card>
+  </v-flex>
 </template>
 
 <script>
-	import Axios from 'axios'
-	import { mapActions } from 'vuex'
+  import { mapActions } from 'vuex'
 
-	export default {
-		name: 'create-monster-tracker',
-		data() {
-			return {
-				valid: null,
-				name: null,
-				maxHealth: null,
-				isBoss: false,
-			}
-		},
-		methods: {
-			...mapActions(['saveMonster']),
-			addMonster() {
-				let _this = this
+  export default {
+    name: 'create-monster-tracker',
+    data () {
+      return {
+        valid: null,
+        name: null,
+        maxHealth: null,
+        isBoss: false,
+      }
+    },
+    methods: {
+      ...mapActions(['saveMonster']),
+      addMonster () {
+        let _this = this
 
-				// Get the user_id and encounter_id
-				const user_id = localStorage.getItem('user_id') ? localStorage.getItem('user_id') : this.$route.params.id
-				const encounter_id = localStorage.getItem('encounter_id') ? localStorage.getItem('encounter_id') : this.$route.params.encounterId
+        // Get the user_id and encounter_id
+        const user_id = localStorage.getItem('user_id') ? localStorage.getItem('user_id') : this.$route.params.id
+        const encounter_id = localStorage.getItem('encounter_id') ? localStorage.getItem('encounter_id') : this.$route.params.encounterId
 
-				// setup monster creation command
-				const command = {
-					name: this.name,
-					is_boss: this.isBoss,
-					max_health: this.maxHealth,
-					current_health: this.maxHealth,
-					encounter_id,
-					user_id,
-				};
+        // setup monster creation command
+        const command = {
+          name: this.name,
+          is_boss: this.isBoss,
+          max_health: this.maxHealth,
+          current_health: this.maxHealth,
+          encounter_id,
+          user_id,
+        }
 
-				// Execute command
-				this.saveMonster(command)
-			}
-		}
-	}
+        // Execute command
+        this.saveMonster(command)
+      },
+    },
+  }
 </script>
--- src/vue/views/Monsters.vue
+++ src/vue/views/Monsters.vue
@@ -1,116 +1,114 @@
 <template>
-	<v-layout
-		row
-		wrap
-	>
-		<v-flex xs12>
-			<router-link
-				:to="`/user/${$route.params.id}/encounters`"
-				@click.native="backToEncounters()"
-			>Back to encounters</router-link>
+  <v-layout
+    row
+    wrap
+  >
+    <v-flex xs12>
+      <router-link
+        :to="`/user/${$route.params.id}/encounters`"
+        @click.native="backToEncounters()"
+      >
+        Back to encounters
+      </router-link>
 
-			<v-layout
-				row
-				wrap
-			>
-				<create-monster-tracker />
-			</v-layout>
+      <v-layout
+        row
+        wrap
+      >
+        <create-monster-tracker />
+      </v-layout>
 
-			<v-layout
-				align-start
-				justify-start
-				row
-				wrap
-			>
-				<edit-monster-tracker
-					v-for="(monster, i) in stateMonsters"
-					:key="i"
-					:monster="monster"
-				/>
-			</v-layout>
-		</v-flex>
-	</v-layout>
+      <v-layout
+        align-start
+        justify-start
+        row
+        wrap
+      >
+        <edit-monster-tracker
+          v-for="(monster, i) in stateMonsters"
+          :key="i"
+          :monster="monster"
+        />
+      </v-layout>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
-	import { mapGetters, mapActions } from 'vuex'
-	import CreateMonsterTracker from '../components/CreateMonsterTracker.vue'
-	import EditMonsterTracker from '../components/EditMonsterTracker.vue'
+  import { mapGetters, mapActions } from 'vuex'
+  import CreateMonsterTracker from '../components/CreateMonsterTracker.vue'
+  import EditMonsterTracker from '../components/EditMonsterTracker.vue'
 
-	export default {
-		name: 'monsters',
+  export default {
+    name: 'monsters',
 
-		components: {
-			CreateMonsterTracker,
-			EditMonsterTracker,
-		},
+    components: {
+      CreateMonsterTracker,
+      EditMonsterTracker,
+    },
 
-		data: () => ({
-			encounterMonsters: [],
-		}),
+    data: () => ({
+      encounterMonsters: [],
+    }),
 
 
-		computed: {
-			...mapGetters(['MONSTERS']),
-			currentPath() {
-				return this.$route.params.id
-			},
+    computed: {
+      ...mapGetters(['MONSTERS']),
+      currentPath () {
+        return this.$route.params.id
+      },
 
-			stateMonsters() {
-				return this.MONSTERS
-			},
-		},
+      stateMonsters () {
+        return this.MONSTERS
+      },
+    },
 
-		watch: {
-			'$route.params.id': function () {
-				this.correctUserRedirect()
-			},
+    watch: {
+      '$route.params.id': function () {
+        this.correctUserRedirect()
+      },
 
-			'$route.params.encounterId': function () {
-				this.correctUserRedirect()
-			}
-		},
+      '$route.params.encounterId': function () {
+        this.correctUserRedirect()
+      }
+    },
 
-		created() {
-			this.correctUserRedirect()
-		},
+    created () {
+      this.correctUserRedirect()
+    },
 
-		mounted() {
-			console.log('fe:', this.stateMonsters)
-		},
+    methods: {
+      ...mapActions(['setLoadingState']),
+      ...mapActions('monsters', ['getMonsters']),
+      correctUserRedirect () {
+        const params_id = this.$route.params.id
+        const storage_user_id = localStorage.getItem('user_id')
+        const encounter_id = localStorage.getItem('encounter_id')
 
-		methods: {
-			...mapActions(['setLoadingState']),
-			...mapActions('monsters', ['getMonsters']),
-			correctUserRedirect() {
-				const params_id = this.$route.params.id
-				const storage_user_id = localStorage.getItem('user_id')
-				const encounter_id = localStorage.getItem('encounter_id')
+        if (params_id == localStorage.getItem('user_id') &&
+          this.$route.params.encounterId === encounter_id) {
+          console.log('true')
+          this.getMonsters()
+        }
+        else {
+          this.$router.push({ path: `/user/${storage_user_id}/encounters/${encounter_id}` })
+        }
+      },
 
-				if (params_id == localStorage.getItem('user_id') &&
-					this.$route.params.encounterId === encounter_id) {
-					console.log('true')
-					this.getMonsters()
-				}
-				else {
-					this.$router.push({ path: `/user/${storage_user_id}/encounters/${encounter_id}` })
-				}
-			},
+      backToEncounters () {
+        this.setLoadingState(true)
+      },
 
-			backToEncounters() {
-				this.setLoadingState(true)
-			},
+      getMonsters () {
+        const user_id = this.$route.params.id
+        const encounter_id = this.$route.params.encounterId
 
-			getMonsters() {
-				const user_id = this.$route.params.id
-				const encounter_id = this.$route.params.encounterId
-
-				const command = {
-					user_id,
-					encounter_id,
-				}
-				this.$store.dispatch('getMonsters', command)
-			},
-		},
-	}
+        const command = {
+          user_id,
+          encounter_id,
+        }
+        this.$store.dispatch('getMonsters', command)
+      },
+    },
+  }
 </script>
