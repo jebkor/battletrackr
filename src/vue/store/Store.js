e257199ce0962a95bb64ef18e6f89170db26e3f6
jebkor <jesper@jebkor.dk>
2019-06-09 16:04:11 +0200

    Fix linting errors in Store.js

--- src/vue/store/Store.js
+++ src/vue/store/Store.js
@@ -1,68 +1,60 @@
-import Vue from "vue";
-import Vuex from "vuex";
-import Axios from "axios";
+import Vue from 'vue';
+import Vuex from 'vuex';
 
 import sharedModule from './modules/shared'
 import monstersModule from './modules/monsters'
 import encountersModule from './modules/encounters'
 
-Vue.use(Vuex);
+Vue.use(Vuex)
 
 
+const store = new Vuex.Store({
+  modules: {
+    shared: sharedModule,
+    monsters: monstersModule,
+    encounters: encountersModule,
+  },
 
-let store = new Vuex.Store({
-	modules: {
-		shared: sharedModule,
-		monsters: monstersModule,
-		encounters: encountersModule
-	},
+  // The state that contains the data
+  state: {
+    loading: false,
+    darkTheme: false,
+  },
 
-	// The state that contains the data
-	state: {
-		loading: false,
-		darkTheme: false
-	},
+  // Mutations set/change the state, should ideally need actions to run
+  mutations: {
+    setDarkTheme (state, darkTheme) {
+      state.darkTheme = darkTheme
+    },
 
-	// Mutations set/change the state, should ideally need actions to run
-	mutations: {
-		setDarkTheme(state, darkTheme) {
-			state.darkTheme = darkTheme
-		},
+    changeLoadingState (state, loading) {
+      state.loading = loading
+    },
+  },
 
-		changeLoadingState(state, loading) {
-			state.loading = loading
-		},
-	},
+  // Actions call a mutation with data as a param, in order to save/change data
+  // Can use ...mapActions([])
+  actions: {
+    setDarkTheme ({
+      commit,
+    }, darkTheme) {
+      commit('setDarkTheme', darkTheme)
+    },
 
-	// Actions call a mutation with data as a param, in order to save/change data
-	// Can use ...mapActions([])
-	actions: {
-		setDarkTheme({
-			commit,
-			dispatch
-		}, darkTheme) {
-			commit('setDarkTheme', darkTheme)
-		},
+    setLoadingState ({
+      commit,
+    }, loading) {
+      commit('changeLoadingState', loading)
+    },
+  },
 
-		setLoadingState({
-			commit,
-			dispatch
-		}, loading) {
-			commit('changeLoadingState', loading)
-		},
-	},
+  // Used in the frontend to display the data
+  // Can use ...mapGetters([])
+  getters: {
+    DARKTHEME: state => state.darkTheme,
 
-	// Used in the frontend to display the data
-	// Can use ...mapGetters([])
-	getters: {
-		DARKTHEME: state => {
-			return state.darkTheme
-		},
+    LOADING: state => state.loading,
+  },
+})
 
-		LOADING: state => {
-			return state.loading
-		},
-	},
-});
-
-export default store;
+export default store
