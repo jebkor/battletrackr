c7ad712147db14c7bbf947f68438ccd9c2c75e44
jebkor <jesper@jebkor.dk>
2019-06-25 21:50:23 +0200

    Update the application with a footer
    * Change minor folder structure
    * Seperate navigation drawer into own file
    * Add footer
    * Start on links in the footer

---
+++ src/vue/components/organisms/AppFooter/AppFooter.vue
@@ -0,0 +1,94 @@
+<template>
+  <v-footer class="pa-3">
+    <v-container grid-list-md>
+      <v-layout
+        row
+        wrap
+      >
+        <v-flex
+          xs12
+          lg3
+        >
+          <h2>Navigation</h2>
+          <ul>
+            <li>
+              <router-link
+                title="Go to encounters"
+                :to="`/user/${userId}/encounters`"
+              >
+                Encounters
+              </router-link>
+            </li>
+          </ul>
+        </v-flex>
+
+        <v-flex
+          xs12
+          lg3
+        >
+          <h2>Support</h2>
+          <ul>
+            <li><router-link title="Go to the FAQ" :to="'/faq'">FAQ</router-link></li>
+            <li><a href="#">Help</a></li>
+          </ul>
+        </v-flex>
+
+        <v-flex
+          xs12
+          lg3
+        >
+          <h2>App</h2>
+          <ul>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Releases</a></li>
+            <li><a href="#">Roadmap</a></li>
+            <li><a href="#">Issues</a></li>
+          </ul>
+        </v-flex>
+
+        <v-flex
+          xs12
+          lg3
+        >
+          <h2>Social</h2>
+          <ul>
+            <li><a href="#">Patreon</a></li>
+            <li><a href="#">Twitter</a></li>
+            <li><a href="#">GitHub</a></li>
+          </ul>
+        </v-flex>
+      </v-layout>
+
+      <v-layout
+        row
+        wrap
+      >
+        <v-spacer />
+        <div>jebkor &copy; {{ new Date().getFullYear() }}</div>
+      </v-layout>
+    </v-container>
+  </v-footer>
+</template>
+
+<script>
+  import userAuthMixin from '../../../mixins/userAuthMixin'
+
+
+  export default {
+    name: 'AppFooter',
+
+    mixins: [userAuthMixin],
+
+    data: () => ({
+
+    }),
+
+    computed: {
+      userId () {
+        return localStorage.user_id
+      },
+    },
+  }
+</script>
+
+<style src="./AppFooter.scss" />
